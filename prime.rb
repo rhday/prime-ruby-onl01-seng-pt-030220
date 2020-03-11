78f9c26c8f671a5bbbf66a90791ce623e7d414e5
Ruari Day <ruariday@gmail.com>
2020-03-11 13:34:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(numbers)
+  (2..(numbers - 1)).each do |num|
+    return false if numbers % num == 0
+  end
+  true
+end
+=begin
+def prime?(numbers)
+  (2..numbers - 1).each {|num| return false if (numbers % num) == 0 || numbers <= 0}
+  true
+end
+##This doesn't account for negative numbers
+=end 
+#if (numbers % num) - (interger numbers % num) true = prime number
