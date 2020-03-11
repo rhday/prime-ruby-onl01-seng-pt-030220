f81d0cb0800ce25a028cd0a251a5db5d47961e60
Ruari Day <ruariday@gmail.com>
2020-03-11 13:40:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,14 @@
 
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  else (2..number/2).none? do |num|
+    number % num == 0
+  end
+  end
+end
 =begin
 def prime?(numbers)
   (2..(numbers - 1)).each do |num|
