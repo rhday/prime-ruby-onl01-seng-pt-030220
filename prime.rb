d2c8dd63fcd335928f3f13a0a6baa8dff15ca8c6
Ruari Day <ruariday@gmail.com>
2020-03-11 13:42:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
-# Add  code here!
+
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  else (2..number/2).none? do |num|
+    number % num == 0
+  end
+  end
+end
+=begin
+def prime?(numbers)
+  (2..(numbers - 1)).each do |num|
+    return false if numbers % num
+  end
+  true
+end
+
+def prime?(numbers)
+  (2..numbers - 1).each {|num| return false if (numbers % num) == 0 || numbers <= 0}
+  true
+end
+##This doesn't account for negative numbers
+=end 
+#if (numbers % num) - (interger numbers % num) true = prime number
